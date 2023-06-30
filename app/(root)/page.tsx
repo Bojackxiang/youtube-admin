dd388396371bf1c71c2e584520d85b75c06fde18
Alex <alex@q-83.com>
2023-06-30 15:54:54 +1000

    setup: import shadui into the project

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <section>
+      <div>
+        <Button size={"lg"} variant="destructive">Button</Button>
+      </div>
+      
+    </section>
+  )
+}
