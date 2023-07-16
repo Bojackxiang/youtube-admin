0c2b74146409f42be78c5b88fc7e2b8b5ec55e2e
Alex <alex@q-83.com>
2023-07-16 17:56:16 +1000

    update the middleware to make api public

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/api/:path*"],
+});
 
 // 在这边决定哪一个 page 进入的话要 redirect to login page
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
